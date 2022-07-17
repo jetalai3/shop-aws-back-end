f8589047fdf027f42a95c11ba25c2e701f002c9a
Viktar Svintsitski <jetalai3@gmail.com>
2022-07-17 21:46:58 +0200

    module-3: add getProductsList tests

--- product-service/src/functions/getProductById/getProductById.test.ts
+++ product-service/src/functions/getProductById/getProductById.test.ts
@@ -8,10 +8,10 @@ describe('test getProductById function', () => {
     const contextMock: any = {};
     it('should return 200 when product was found', async () => {
         expect((await getProductById(eventMock, contextMock)).statusCode).toEqual(200);
-    }),
+    })
     it('should return correct product instance', async () => {
         expect(JSON.parse((await getProductById(eventMock, contextMock)).body).data).toEqual(products[0]);
-    }),
+    })
     it('should return 404 when product was not found', async () => {
         expect((await getProductById({ pathParameters: { id: 1 }}, contextMock)).statusCode).toEqual(404);
     })
