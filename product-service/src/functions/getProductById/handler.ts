4d4e5059a400686842a1b7761d22f99e7a48e3d5
Viktar Svintsitski <jetalai3@gmail.com>
2022-07-17 20:03:19 +0200

    module-3: add getProductById error handling

--- product-service/src/functions/getProductById/handler.ts
+++ product-service/src/functions/getProductById/handler.ts
@@ -1,16 +1,22 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
+import { ProductNotFoundError } from 'src/errors/productNotFound';
 import getProducts from 'src/utils/getProducts';
 
 import schema from './schema';
 
 const getProductById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  const productslist = await getProducts();
-  const product = productslist.find(el => el.id === event.pathParameters.id);
-  return formatJSONResponse({
-    data: product
-  });
+  try {
+    const productslist = await getProducts();
+    const product = productslist.find(el => el.id === event.pathParameters.id);
+    if (!product) throw new ProductNotFoundError();
+    return formatJSONResponse({
+      data: product
+    });
+  } catch (err) {
+    return err instanceof ProductNotFoundError ? formatJSONResponse({ message: err.message }, 404) : formatJSONResponse({ message: 'Unknown error.' }, 500);
+  };
 };
 
 export const main = middyfy(getProductById);
