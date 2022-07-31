e15b3df25d6bbbc02eb2eba78bd5f660777af174
Viktar Svintsitski <jetalai3@gmail.com>
2022-07-31 13:32:40 +0200

    module-5: add import-service and importProductsFile lambda

---
+++ import-service/serverless.ts
@@ -0,0 +1,80 @@
+import type { AWS } from '@serverless/typescript';
+
+import importProductsFile from '@functions/importProductsFile';
+
+const serverlessConfiguration: AWS = {
+  service: 'import-service',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild'],
+  provider: {
+    name: 'aws',
+    region: 'us-east-1',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+    iamRoleStatements: [
+			{
+				Effect: "Allow",
+				Action: "s3:*",
+				Resource: {
+					"Fn::Join": [
+						"",
+						[{ "Fn::GetAtt": ["ImportedFilesBucket", "Arn"] }, ""],
+					],
+				},
+			},
+			{
+				Effect: "Allow",
+				Action: "s3:*",
+				Resource: {
+					"Fn::Join": [
+						"",
+						[{ "Fn::GetAtt": ["ImportedFilesBucket", "Arn"] }, "/*"],
+					],
+				},
+			},
+		],
+  },
+  // import the function via paths
+  functions: { importProductsFile },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+  resources: {
+		Resources: {
+			ImportedFilesBucket: {
+				Type: 'AWS::S3::Bucket',
+				Properties: {
+					BucketName: 'import-service-bucket-jetalai',
+					CorsConfiguration: {
+						CorsRules: [
+							{
+								AllowedHeaders: ['*'],
+								AllowedMethods: ['PUT'],
+								AllowedOrigins: ['*'],
+							},
+						],
+					},
+				},
+			},
+		},
+  },
+};
+
+module.exports = serverlessConfiguration;
---
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -0,0 +1,35 @@
+import { S3 } from "aws-sdk";
+
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+
+import schema from './schema';
+
+const importProductsFile: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  try {
+    const fileName = event.queryStringParameters.name;
+
+    const s3Instance = new S3({ region: 'us-east-1' });
+
+    const urlForUpload = await s3Instance.getSignedUrlPromise("putObject", {
+      Bucket: 'import-service-bucket-jetalai',
+      Key: `uploaded/${fileName}`,
+      Expires: 60,
+      ContentType: "text/csv",
+    });
+
+    return formatJSONResponse({
+      urlForUpload,
+    });
+  } catch (error) {
+    return formatJSONResponse(
+      {
+        message: "Unknown error",
+      },
+      500
+    );
+  }
+};
+
+export const main = middyfy(importProductsFile);
